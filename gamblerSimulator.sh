3df0a72111352e9ab969ff0a8ff7f57d9bbb4a5a
smitagaikwad25 <gaikwadsmita021@gmail.com>
2019-11-23 15:40:27 +0530

    Add:gamblerWonOrLost%0%Stake

--- gamblerSimulator.sh
+++ gamblerSimulator.sh
@@ -4,13 +4,29 @@ echo "WelCome in gambler simulator"
 #Constant
 STAKE=100
 BET=1
+#variables
 
+res=0
+res1=0
+cash=$STAKE
+function gameStart()	
+	{
+		while [ $cash -lt 150 ] && [ $cash -gt 50 ]
+		do
 
-	random=$((RANDOM%2))
-	if [ $random -eq 1 ]
-	then
-		echo "win :" $(( $STAKE + $BET ))
-	else
-		echo "loss :" $(( $STAKE - $BET ))
-	fi
-	
+			random=$((RANDOM%2))
+			if [ $random -eq 1 ]
+			then
+				echo "win"
+				cash=$(($cash + $BET))
+				echo $cash
+			else
+				echo "loss" 
+				cash=$(( $cash - $BET ))
+				echo $cash
+			fi
+
+		done
+	}
+
+gameStart
